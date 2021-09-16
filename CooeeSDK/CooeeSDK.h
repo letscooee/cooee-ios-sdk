9497f8dda41e1fdb2f3283ec68d93ac5d53650cd
Ashish Gaikwad <ashish@letscooee.com>
2021-09-16 11:11:31 +0530

    Initial Commit

---
+++ CooeeSDK/CooeeSDK.h
@@ -0,0 +1,18 @@
+//
+//  CooeeSDK.h
+//  CooeeSDK
+//
+//  Created by Ashish Gaikwad on 16/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CooeeSDK.
+FOUNDATION_EXPORT double CooeeSDKVersionNumber;
+
+//! Project version string for CooeeSDK.
+FOUNDATION_EXPORT const unsigned char CooeeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CooeeSDK/PublicHeader.h>
+
+
