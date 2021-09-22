e2de5f44314f088d49d9a14d85c08185cf0c9828
Ashish Gaikwad <ashish@letscooee.com>
2021-09-22 13:20:35 +0530

    refactor: Update Event and Introduce USerAuthResponse model COOEE-10

--- CooeeSDK/models/Event.swift
+++ CooeeSDK/models/Event.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-struct Event: Decodable {
+struct Event {
     // MARK: Lifecycle
 
     init(eventName: String) {
-        self.init(eventName: eventName, properties: {})
+        self.init(eventName: eventName, properties: [String: Any]())
     }
 
     init(eventName: String, triggerData: TriggerData) {
         self.init(eventName: eventName)
     }
 
-    init(eventName: String, properties: [String, Any]) {
+    init(eventName: String, properties: [String: Any]) {
         name = eventName
         self.properties = properties
         occurred = Date()
@@ -27,15 +27,15 @@ struct Event: Decodable {
     // MARK: Internal
 
     var name: String?
-    var properties: [String, Any]?
+    var properties: [String: Any]?
     var sessionID: String?
     var sessionNumber: Int?
     var screenName: String?
-    var activeTriggers: [[String, Object]]?
+    var activeTriggers: [[String: Any]]?
     var occurred: Date?
-    var deviceProps = [String, Any]?
+    var deviceProps: [String: Any]?
 
-    func withTrigger(triggerData: TriggerData) {
-        properties["triggerID"] = triggerData.triggerID
+    mutating func withTrigger(triggerData: TriggerData) {
+        properties?.updateValue(triggerData.triggerID, forKey: "triggerID")
     }
 }
