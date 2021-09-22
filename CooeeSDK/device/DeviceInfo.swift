9064653c9c41d0ae66bd83bde8584361098cf643
Ashish Gaikwad <ashish@letscooee.com>
2021-09-22 13:16:59 +0530

    refactor: Add more device information in DeviceInfo COOEE-10

--- CooeeSDK/device/DeviceInfo.swift
+++ CooeeSDK/device/DeviceInfo.swift
@@ -5,6 +5,7 @@
 //  Created by Ashish Gaikwad on 16/09/21.
 //
 
+import ARKit
 import Foundation
 import UIKit
 
@@ -15,10 +16,21 @@ import UIKit
  - Since:0.1
  */
 class DeviceInfo {
-    static let shared = DeviceInfo()
-    let cachedInfo = CashedInfo()
-    
     struct CashedInfo {
+        // TODO: blutute
+    
+        var manufacture = "Apple"
+        // var networkUtility = NetworkUtility.shared
+        var deviceBattery = UIDevice.current.batteryLevel * 100
+        // var networkProvider = NetworkUtility.shared.getCarrierName()
+        // var networkType = NetworkUtility.shared.getNetworkType()
+        
+        let osVersion = String(ProcessInfo().operatingSystemVersion.majorVersion) + "." + String(ProcessInfo().operatingSystemVersion.minorVersion) + "." + String(ProcessInfo().operatingSystemVersion.patchVersion)
+        
+        let totalRAM = ProcessInfo.processInfo.physicalMemory/1024/1024
+        let availavleRAM = 1234
+        let dpi = 1.0
+
         var name: String {
             return UIDevice.current.name
         }
@@ -27,10 +39,48 @@ class DeviceInfo {
             return UIScreen.main.bounds.width
         }
         
-        public var height: CGFloat {
+        var height: CGFloat {
             return UIScreen.main.bounds.height
         }
+
+        var arSupport: Bool {
+            return ARConfiguration.isSupported
+        }
+        
+        var deviceOrientation: String {
+            return UIDevice.current.orientation.isLandscape ? "Landscape" : "Potrait"
+        }
+        
+        var deviceModel: String {
+            return UIDevice.current.model
+        }
+        
+        var freeSpace: Int64 {
+            do {
+                guard let totalDiskSpaceInBytes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())[FileAttributeKey.systemFreeSize] as? Int64 else {
+                    return 0
+                }
+                return totalDiskSpaceInBytes
+            } catch {
+                return 0
+            }
+        }
+        
+        var totalSpace: Int64 {
+            do {
+                guard let totalDiskSpaceInBytes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())[FileAttributeKey.systemSize] as? Int64 else {
+                    return 0
+                }
+                return totalDiskSpaceInBytes
+            } catch {
+                return 0
+            }
+        }
     }
+
+    static let shared = DeviceInfo()
+
+    let cachedInfo = CashedInfo()
 
     /**
       Get the name of the device.
