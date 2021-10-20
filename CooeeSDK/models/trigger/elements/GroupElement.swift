a45825c46ee37f780132da2126d2719cd0246ff6
Ashish Gaikwad <ashish@letscooee.com>
2021-10-20 18:18:50 +0530

    feat: Introduce Button, Group, Image, Text & Video Elements COOEE-116

---
+++ CooeeSDK/models/trigger/elements/GroupElement.swift
@@ -0,0 +1,19 @@
+//
+// Created by Ashish Gaikwad on 19/10/21.
+//
+
+import Foundation
+
+/**
+ - Author: Ashish Gaikwad
+ - Since: 0.1.0
+ */
+class GroupElement: BaseElement {
+    // MARK: Lifecycle
+
+    required init() {}
+
+    // MARK: Internal
+
+    var children: [BaseElement]?
+}
---
+++ CooeeSDK/models/trigger/elements/ImageElement.swift
@@ -0,0 +1,19 @@
+//
+// Created by Ashish Gaikwad on 19/10/21.
+//
+
+import Foundation
+
+/**
+ - Author: Ashish Gaikwad
+ - Since: 0.1.0
+ */
+class ImageElement: BaseElement {
+    // MARK: Lifecycle
+
+    required init() {}
+
+    // MARK: Internal
+
+    var url: String?
+}
