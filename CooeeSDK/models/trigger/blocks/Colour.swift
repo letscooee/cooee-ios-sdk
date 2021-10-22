04c51934b75953712108cef944d256a5e6730db5
Ashish Gaikwad <ashish@letscooee.com>
2021-10-22 17:20:25 +0530

    refactor: Add getColour Method to get UIColor COOEE-116

--- CooeeSDK/models/trigger/blocks/Colour.swift
+++ CooeeSDK/models/trigger/blocks/Colour.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 import HandyJSON
+import UIKit
 
 /**
  - Author: Ashish Gaikwad
  - Since: 0.1.0
  */
 struct Colour: HandyJSON {
-    
+
     var hex: String?
     var grad: Gradient?
+
+    func getColour() -> UIColor {
+        UIColor(hexString: hex!)
+    }
 }
