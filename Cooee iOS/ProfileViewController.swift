bb8f7d9d86a915b5421d77610a35b53a7bf86e85
Ashish Gaikwad <ashish@letscooee.com>
2021-10-28 11:02:32 +0530

    refactor: Update Internal app to update profile COOEE-116

---
+++ Cooee iOS/ProfileViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileViewController.swift
+//  Cooee iOS
+//
+//  Created by Ashish Gaikwad on 27/10/21.
+//
+
+import UIKit
+import CooeeSDK
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var edtName: UITextField!
+    @IBOutlet weak var edtEmail: UITextField!
+    @IBOutlet weak var edtMobile: UITextField!
+
+    var cooeeSDK = CooeeSDK.getInstance()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func saveProfile(_ sender: Any) {
+        let name = edtName.text
+        let email = edtEmail.text
+        let mobile = edtMobile.text
+
+        if name?.isEmpty ?? true {
+            showAlert("Name can't be empty", false)
+        }
+
+        if email?.isEmpty ?? true {
+            showAlert("Email can't be empty", false)
+        }
+
+        if mobile?.isEmpty ?? true {
+            showAlert("Mobile No. can't be empty", false)
+        }
+
+        cooeeSDK.updateUserData(userData: ["name": name!,
+                                           "email": email!,
+                                           "mobile": mobile!])
+        showAlert("Profile Updated", true)
+    }
+
+    private func showAlert(_ msg: String, _ close: Bool) {
+        let alert = UIAlertController(title: "Warning", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { action in
+            if close {
+                self.presentingViewController?.dismiss(animated: true)
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
