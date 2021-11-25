3c4c92306acdecb0dfb02fa446779ae66a9d989f
Ashish Gaikwad <ashish@letscooee.com>
2021-11-25 10:05:43 +0530

    refactor: Add some id, duration Information to JSON COOEE-453

--- Cooee iOS/ViewController.swift
+++ Cooee iOS/ViewController.swift
@@ -5,7 +5,9 @@
 //  Created by Ashish Gaikwad on 16/09/21.
 //
 
+import BSON
 import CooeeSDK
+import HandyJSON
 import UIKit
 
 class ViewController: UIViewController {
@@ -22,18 +24,31 @@ class ViewController: UIViewController {
 
     @IBAction func loadPayload(_ sender: Any) {
         do {
-                if let bundlePath = Bundle.main.url(forResource: "samplepayloadone",
-                                                    withExtension: "json"){
-                    print("path obtained")
-                    let jsonData = try Data(contentsOf: bundlePath)
-                    let rawString = String(data: jsonData, encoding: .utf8)
-                    let formatedString = rawString?.trimmingCharacters(in: .whitespacesAndNewlines)
-                    EngagementTriggerHelper.renderInAppTriggerFromJSONString(rawString!)
-                }else{
-                    print("file not found")
-                }
-            } catch {
-                print(error)
+            if let bundlePath = Bundle.main.url(forResource: "samplepayloadone",
+                                                withExtension: "json")
+            {
+                print("path obtained")
+                let jsonData = try Data(contentsOf: bundlePath)
+
+                var json = try JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) as? [String: Any]
+                json?.updateValue(ObjectId().hexString, forKey: "id")
+                json?.updateValue(ObjectId().hexString, forKey: "engagementID")
+                json?.updateValue((Date().timeIntervalSince1970*1000) + (5*60*1000), forKey: "expireAt")
+
+                let rawString = String(data: json!.percentEncoded()!, encoding: .utf8)
+                EngagementTriggerHelper.renderInAppTriggerFromJSONString(rawString!)
+            } else {
+                print("file not found")
             }
+        } catch {
+            print(error)
+        }
+    }
+}
+
+public extension Dictionary {
+    func percentEncoded() -> Data? {
+        let jsonData = try! JSONSerialization.data(withJSONObject: self, options: JSONSerialization.WritingOptions.prettyPrinted)
+        return jsonData
     }
 }
