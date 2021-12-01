57331cc878997986934d0420ba6de98db8bc534c
Ashish Gaikwad <ashish@letscooee.com>
2021-12-01 14:33:38 +0530

    refactor: Disable Sentry debug mode to prevent unwanted logs COOEE-111

--- CooeeSDK/utils/SentryHelper.swift
+++ CooeeSDK/utils/SentryHelper.swift
@@ -104,7 +104,7 @@ class SentryHelper {
 
         SentrySDK.start { options in
             options.dsn = SentryHelper.COOEE_DSN
-            options.debug = self.sdkInfo.cachedInfo.isDebugging
+            options.debug = false
             options.releaseName = "com.letscooee@\(self.sdkInfo.cachedInfo.sdkVersion)+\(self.sdkInfo.cachedInfo.sdkLongVersion)"
             options.environment = self.sdkInfo.cachedInfo.isDebugging ? "development" : "production"
             self.setupFilterToExcludeNonCooeeEvents(options)
