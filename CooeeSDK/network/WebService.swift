fa604bd3a7dfdbed5b6830c1354448421e180fb7
Ashish Gaikwad <ashish@letscooee.com>
2021-12-03 12:36:21 +0530

    refactor: Update WebService to return Dictionary of string,any COOEE-111

--- CooeeSDK/network/WebService.swift
+++ CooeeSDK/network/WebService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import HandyJSON
 
 /**
  Send any request to server
@@ -18,7 +19,7 @@ class WebService: NSObject {
 
     static let shared = WebService()
 
-    func getResponse<T: Decodable>(fromURL: String, method: httpMethod, params: [String: Any?], header: [String: String], t: T.Type) throws -> T? {
+    func getResponse(fromURL: String, method: httpMethod, params: [String: Any?], header: [String: String]) throws -> [String:Any]? {
         let url = URL(string: getCompleteURL(url: fromURL))!
         let group = DispatchGroup()
         var request = URLRequest(url: url)
@@ -46,26 +47,25 @@ class WebService: NSObject {
         }
         task.resume()
         group.wait()
-        return try processResponse(responseData, responseError, t)
+        return try processResponse(responseData, responseError)
     }
 
     // MARK: Private
 
-    private func processResponse<T: Decodable>(_ data: Data?, _ error: Error?, _ t: T.Type) throws -> T? {
+    private func processResponse(_ data: Data?, _ error: Error?) throws -> [String:Any]? {
         guard let data = data, error == nil
         else {
             print("error", error ?? "Unknown error")
             throw error!
         }
-        let responseString = String(data: data, encoding: .utf8)
+        let responseString = String(data: data, encoding: .utf8)?.convertToDictionary()
         print("""
               \n-------WS Response--------\n
-              \(responseString ?? "")\n
+              \(String(describing: responseString))\n
               -------End WS Response--------
               """)
 
-        let decodedResponse = try? JSONDecoder().decode(t.self, from: data)
-        return decodedResponse
+        return responseString
 
     }
 
