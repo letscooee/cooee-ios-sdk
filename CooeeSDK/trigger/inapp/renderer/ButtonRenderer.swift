82746a35f000492fe815c3198fa1989c17d2d5a7
Ashish Gaikwad <ashish@letscooee.com>
2022-01-17 10:38:04 +0530

    refactor 📦: Update ButtonRenderer for corner radius issue COOEE-541

--- CooeeSDK/trigger/inapp/renderer/ButtonRenderer.swift
+++ CooeeSDK/trigger/inapp/renderer/ButtonRenderer.swift
@@ -24,8 +24,34 @@ struct ButtonRenderer: View {
 
     let parentTextElement: ButtonElement
     let triggerContext: TriggerContext
+    @State var childSize: CGSize = .zero
 
     var body: some View {
-        TextRenderer(parentTextElement, triggerContext)
+        let alignment = parentTextElement.getSwiftUIAlignment()
+        let textAlignment = parentTextElement.getTextAlignment()
+
+        // Reference to add GeometryReader & onPreferenceChange which will help to keep watch on height of element
+        // https://stackoverflow.com/a/56782264/9256497
+
+        parentTextElement.getSinglePart()
+                .background(
+                        GeometryReader { proxy in
+                            Color.clear
+                                    .preference(
+                                    key: SizePreferenceKey.self,
+                                    value: proxy.size
+                            )
+                        }
+                )
+                .fixedSize(horizontal: false, vertical: true)
+                .multilineTextAlignment(textAlignment)
+                .if(parentTextElement.getCalculatedWidth() != nil) {
+                    $0.frame(width: parentTextElement.getCalculatedWidth()!, height: childSize.height, alignment: alignment)
+                }
+                .frame(alignment: alignment)
+                .onPreferenceChange(SizePreferenceKey.self) { preferences in
+                    let _ = print("height: \(preferences.height) width: \(preferences.width)")
+                    self.childSize = preferences
+                }
     }
 }
