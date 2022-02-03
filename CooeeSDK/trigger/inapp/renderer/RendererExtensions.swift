feda6e1d62d5e261ac2720989f975f145df4ce6c
Ashish Gaikwad <ashish@letscooee.com>
2022-02-03 14:48:57 +0530

    refactor 📦: Add UIImage extension to process GIF and make animated UIImage COOEE-563

--- CooeeSDK/trigger/inapp/renderer/RendererExtensions.swift
+++ CooeeSDK/trigger/inapp/renderer/RendererExtensions.swift
@@ -99,6 +99,7 @@ public extension View {
 
      - Parameters:
        - condition: Any user condition
+       - transform: complete listener
      - Returns: View to which condition is added
      */
     @ViewBuilder
@@ -187,5 +188,205 @@ extension UIFont {
             NSLog("Error registering font: \(error.debugDescription)")
             return
         }
+    }
+}
+
+extension UIImage {
+
+    /**
+     Change the size of image present UIImage
+
+     - Parameter sizeChange: custom size of image in CGSize
+     - Returns: resized UIImage
+     */
+    func imageResize(sizeChange: CGSize) -> UIImage {
+        let hasAlpha = true
+        let scale: CGFloat = 0.0 // Use scale factor of main screen
+
+        UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
+        draw(in: CGRect(origin: CGPoint.zero, size: sizeChange))
+
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage!
+    }
+
+    /**
+     Check if image has multiple frames. If multiple frames are present then that image is
+     considered as GIF otherwise normal image.
+
+     - Parameter data: image in Data
+     - Returns: optional UIImage
+     */
+    public class func gif(data: Data) -> UIImage? {
+        // Create source from data
+        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else {
+            NSLog("Load GIF/Image: Source for the image does not exist")
+            return nil
+        }
+
+        let count = CGImageSourceGetCount(source)
+        if count > 1 {
+            return UIImage.animatedImageWithSource(source)
+        } else {
+            return UIImage(data: data)
+        }
+    }
+
+    @available(iOS 9.0, *)
+    public class func gif(asset: String) -> UIImage? {
+        // Create source from assets catalog
+        guard let dataAsset = NSDataAsset(name: asset) else {
+            NSLog("Load GIF/Image: Cannot turn image named \"\(asset)\" into NSDataAsset")
+            return nil
+        }
+
+        return gif(data: dataAsset.data)
+    }
+
+    /**
+     Evaluate the display delay for particular frame
+
+     - Parameters:
+       - index: position of the frame to whom delay to be added
+       - source: source ig GIF image as CGSourceImage
+     - Returns: double value how much delay should be added to the particular frame
+     */
+    internal class func delayForImageAtIndex(_ index: Int, source: CGImageSource!) -> Double {
+        var delay = 0.1
+
+        // Get dictionaries
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifPropertiesPointer = UnsafeMutablePointer<UnsafeRawPointer?>.allocate(capacity: 0)
+        defer {
+            gifPropertiesPointer.deallocate()
+        }
+        let unsafePointer = Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()
+        if CFDictionaryGetValueIfPresent(cfProperties, unsafePointer, gifPropertiesPointer) == false {
+            return delay
+        }
+
+        let gifProperties: CFDictionary = unsafeBitCast(gifPropertiesPointer.pointee, to: CFDictionary.self)
+
+        // Get delay time
+        var delayObject: AnyObject = unsafeBitCast(
+                CFDictionaryGetValue(gifProperties,
+                        Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()),
+                to: AnyObject.self
+        )
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties,
+                    Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
+        }
+
+        if let delayObject = delayObject as? Double, delayObject > 0 {
+            delay = delayObject
+        } else {
+            delay = 0.1 // Make sure they're not too fast
+        }
+
+        return delay
+    }
+
+    internal class func gcdForPair(_ lhs: Int?, _ rhs: Int?) -> Int {
+        var lhs = lhs
+        var rhs = rhs
+        // Check if one of them is nil
+        if rhs == nil || lhs == nil {
+            if rhs != nil {
+                return rhs!
+            } else if lhs != nil {
+                return lhs!
+            } else {
+                return 0
+            }
+        }
+
+        // Swap for modulo
+        if lhs! < rhs! {
+            let ctp = lhs
+            lhs = rhs
+            rhs = ctp
+        }
+
+        // Get greatest common divisor
+        var rest: Int
+        while true {
+            rest = lhs! % rhs!
+
+            if rest == 0 {
+                return rhs! // Found it
+            } else {
+                lhs = rhs
+                rhs = rest
+            }
+        }
+    }
+
+    internal class func gcdForArray(_ array: [Int]) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+
+        var gcd = array[0]
+
+        for val in array {
+            gcd = UIImage.gcdForPair(val, gcd)
+        }
+
+        return gcd
+    }
+
+    /**
+     Generate animated instance if the UIImage from the given source.
+     - Parameter source: CGImageSource with will contain all GIF frames
+     - Returns: animated UIImage
+     */
+    internal class func animatedImageWithSource(_ source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImage]()
+        var delays = [Int]()
+
+        // Fill arrays
+        for index in 0..<count {
+            // Add image
+            if let image = CGImageSourceCreateImageAtIndex(source, index, nil) {
+                images.append(image)
+            }
+
+            // At it's delay in cs
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(index),
+                    source: source)
+            delays.append(Int(delaySeconds * 1000.0)) // Seconds to ms
+        }
+
+        // Calculate full duration
+        let duration: Int = {
+            var sum = 0
+
+            for val: Int in delays {
+                sum += val
+            }
+
+            return sum
+        }()
+
+        // Get frames
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+
+        var frame: UIImage
+        var frameCount: Int
+        for index in 0..<count {
+            frame = UIImage(cgImage: images[Int(index)])
+            frameCount = Int(delays[Int(index)] / gcd)
+
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+
+        let animation = UIImage.animatedImage(with: frames,
+                duration: Double(duration) / 1000.0)
+        return animation
     }
 }
