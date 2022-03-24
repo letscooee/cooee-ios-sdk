bc1d951dfdf110434088b81adb8362bdb2daa771
Ashish Gaikwad <ashish@letscooee.com>
2022-03-24 11:18:38 +0530

    refactor 📦: Define separate getPaddingForBorder COOEE-645
    If element has padding 0 will be return in getPaddingForBorder otherwise it will add value size as padding

--- CooeeSDK/models/trigger/blocks/Spacing.swift
+++ CooeeSDK/models/trigger/blocks/Spacing.swift
@@ -74,6 +74,62 @@ struct Spacing: HandyJSON {
         return padding == 0 ? value : padding
     }
 
+    /**
+     Calculates scaled left padding and provide result
+     In case if ``pl`` is nil, process ``p`` and if ``p`` is also nil
+     then process for 0
+     Also check of the value is greater than 0 then return 0, Otherwise return the value
+
+     - Parameter value: value to replace the padding
+     - Returns: padding in ``CGFloat``
+     */
+    public func getPaddingLeftForBorder(replaceWith value: CGFloat = 0) -> CGFloat {
+        let padding = UnitUtil.getScaledPixel(pl ?? p ?? 0)
+        return padding > 0 ? 0 : value
+    }
+
+    /**
+     Calculates scaled right padding and provide result
+     In case if ``pr`` is nil, process ``p`` and if ``p`` is also nil
+     then process for 0
+     Also check of the value is greater than 0 then return 0, Otherwise return the value
+
+     - Parameter value: value to replace the padding
+     - Returns: padding in ``CGFloat``
+     */
+    public func getPaddingRightForBorder(replaceWith value: CGFloat = 0) -> CGFloat {
+        let padding = UnitUtil.getScaledPixel(pr ?? p ?? 0)
+        return padding > 0 ? 0 : value
+    }
+
+    /**
+     Calculates scaled top padding and provide result
+     In case if ``pt`` is nil, process ``p`` and if ``p`` is also nil
+     then process for 0
+     Also check of the value is greater than 0 then return 0, Otherwise return the value
+
+     - Parameter value: value to replace the padding
+     - Returns: padding in ``CGFloat``
+     */
+    public func getPaddingTopForBorder(replaceWith value: CGFloat = 0) -> CGFloat {
+        let padding = UnitUtil.getScaledPixel(pt ?? p ?? 0)
+        return padding > 0 ? 0 : value
+    }
+
+    /**
+     Calculates scaled bottom padding and provide result
+     In case if ``pb`` is nil, process ``p`` and if ``p`` is also nil
+     then process for 0
+     Also check of the value is greater than 0 then return 0, Otherwise return the value
+
+     - Parameter value: value to replace the padding
+     - Returns: padding in ``CGFloat``
+     */
+    public func getPaddingBottomForBorder(replaceWith value: CGFloat = 0) -> CGFloat {
+        let padding = UnitUtil.getScaledPixel(pb ?? p ?? 0)
+        return padding > 0 ? 0 : value
+    }
+
     // MARK: Private
 
     private var p: Float?
