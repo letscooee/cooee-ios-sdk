4f5232e2b9feada599296736fe6cf0d0ccf02508
Ashish Gaikwad <ashish@letscooee.com>
2022-07-01 18:07:24 +0530

    refactor: Manage eventID from SDK COOEE-945

--- CooeeSDK/models/Event.swift
+++ CooeeSDK/models/Event.swift
@@ -28,6 +28,8 @@ struct Event: HandyJSON {
     }
 
     init(eventName: String, properties: [String: Any?]) {
+        self._id = ObjectID().hexString
+        name = eventName
         self.name = eventName
         self.properties = properties
         self.occurred = DateUtils.formatDateToUTCString(date: Date())
@@ -43,6 +45,7 @@ struct Event: HandyJSON {
 
     // MARK: Internal
 
+    var _id: String?
     var name: String?
     var properties: [String: Any?]?
     var sessionID: String?
