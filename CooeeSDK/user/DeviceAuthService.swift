3aed32e1f3a9be581d247cd21b11f960cd7c2a3c
Ashish Gaikwad <ashish@letscooee.com>
2022-08-17 13:24:28 +0530

    refactor: Change device validate response COOEE-525
    1. Rename id with userID.
    2. Split code of storing auth details.

--- CooeeSDK/user/DeviceAuthService.swift
+++ CooeeSDK/user/DeviceAuthService.swift
@@ -58,16 +58,48 @@ class DeviceAuthService {
         self.baseHttp?.commonHeaders.userID = self.userID
         self.sentryHelper.setUserId(id: self.userID!)
 
-        // send screenshot once sdk token is acquired/published
-        if !(sdkToken?.isEmpty ?? true) {
+        /*
+        Send screenshot once sdk token is acquired/published
+        Stopping service as server not accepting screenshot request
+        if !(self.sdkToken?.isEmpty ?? true) {
             DispatchQueue.main.async {
                 ScreenShotUtility.captureAndSendScreenShot()
             }
-        }
+        }*/
     }
 
     func getUserID() -> String? {
         self.userID
+    }
+
+    /**
+     Update auth details if its available.
+     This is mainly used while profile merging.
+
+     - Parameter response: response from server
+     */
+    func checkAndUpdate(_ response: [String: Any]?) {
+        guard let deviceAuthResponse = DeviceAuthResponse.deserialize(from: response) else {
+            return
+        }
+
+        // If sdkToken is present then replace it immediatly
+        if let sdkToken = deviceAuthResponse.sdkToken, !sdkToken.isEmpty {
+            self.setSDKToken(sdkToken: sdkToken)
+        }
+
+        // If userID is present then replace it immediatly
+        if let userID = deviceAuthResponse.userID, !userID.isEmpty {
+            self.setUserID(userID: userID)
+        }
+
+        // If deviceID is present then replace it immediatly
+        if let deviceID = deviceAuthResponse.deviceID, deviceID.isEmpty {
+            self.setDeviceID(deviceID: deviceID)
+        }
+
+        // Send to update the local storage & api client
+        self.updateAPI()
     }
 
     // MARK: Private
@@ -79,6 +111,33 @@ class DeviceAuthService {
     private var deviceID: String?
     private var sdkToken: String?
     private var uuID: String?
+
+    /**
+     Populates value to the ``deviceID`` and LocalStorage immediately.
+     - Parameter deviceID: deviceID to be setÏ
+     */
+    private func setDeviceID(deviceID: String) {
+        self.deviceID = deviceID
+        LocalStorageHelper.putString(key: Constants.STORAGE_DEVICE_ID, value: deviceID)
+    }
+
+    /**
+     Populates value to the ``userID`` and LocalStorage immediately.
+     - Parameter userID: userID to be set
+     */
+    private func setUserID(userID: String) {
+        self.userID = userID
+        LocalStorageHelper.putString(key: Constants.STORAGE_USER_ID, value: userID)
+    }
+
+    /**
+     Populates value to the ``sdkToken`` and LocalStorage immediately.
+     - Parameter sdkToken: sdkToken to be set
+     */
+    private func setSDKToken(sdkToken: String) {
+        self.sdkToken = sdkToken
+        LocalStorageHelper.putString(key: Constants.STORAGE_SDK_TOKEN, value: sdkToken)
+    }
 
     /**
      * This method will pull user data (like SDK token & user ID) from the local storage (shared preference)
@@ -126,49 +185,16 @@ class DeviceAuthService {
         }
     }
 
+    /**
+     Save user data in local storage (shared preference)
+     - Parameter data: data to be saved
+     */
     private func saveUserDataInStorage(data: DeviceAuthResponse) {
-        self.sdkToken = data.sdkToken ?? ""
-        self.userID = data.id ?? ""
-        self.deviceID = data.deviceID ?? ""
+        self.setSDKToken(sdkToken: data.sdkToken!)
+        self.setUserID(userID: data.userID!)
+        self.setDeviceID(deviceID: data.deviceID!)
         self.updateAPI()
 
-        LocalStorageHelper.putString(key: Constants.STORAGE_DEVICE_ID, value: self.deviceID!)
-        LocalStorageHelper.putString(key: Constants.STORAGE_SDK_TOKEN, value: self.sdkToken!)
-        LocalStorageHelper.putString(key: Constants.STORAGE_USER_ID, value: self.userID!)
         LocalStorageHelper.putString(key: Constants.STORAGE_DEVICE_UUID, value: self.uuID!)
-    }
-
-    /**
-     Update auth details if its available.
-     This is mainly used while profile merging.
-
-     - Parameter response: response from server
-     */
-    func checkAndUpdate(_ response: [String: Any]?) {
-        guard let mergeDetails = response?["mergeDetails"] as? [String: Any] else {
-            return
-        }
-
-        guard var deviceAuthResponse = DeviceAuthResponse.deserialize(from: mergeDetails) else {
-            return
-        }
-
-        // If sdkToken is not present then add it from local
-        if deviceAuthResponse.sdkToken?.isEmpty ?? true {
-            deviceAuthResponse.sdkToken = self.sdkToken
-        }
-
-        // If userID is not present then add it from local
-        if deviceAuthResponse.id?.isEmpty ?? true {
-            deviceAuthResponse.id = self.userID
-        }
-
-        // If deviceID is not present then add it from local
-        if deviceAuthResponse.deviceID?.isEmpty ?? true {
-            deviceAuthResponse.deviceID = self.deviceID
-        }
-
-        // Send to update the local storage & api client
-        saveUserDataInStorage(data: deviceAuthResponse)
     }
 }
