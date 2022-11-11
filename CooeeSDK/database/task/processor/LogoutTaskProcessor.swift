24d4541cbfb2ee7b5a56a62ee818b706847e134d
Ashish Gaikwad <ashish@letscooee.com>
2022-11-11 16:49:59 +0530

    feat: Expose LogoutTaskProcessor COOEE-1068

---
+++ CooeeSDK/database/task/processor/LogoutTaskProcessor.swift
@@ -0,0 +1,26 @@
+// 
+// LogoutTaskProcessor.swift
+// CooeeSDK
+//
+// Created by Ashish Gaikwad on 11/11/22.
+//
+
+import Foundation
+import HandyJSON
+
+class LogoutTaskProcessor: HttpTaskProcessor<String> {
+    override func doHttp(data: String) throws {
+        try super.baseHttpService.logOutUser()
+    }
+
+    override func canProcess(_ task: PendingTasks) -> Bool {
+        PendingTaskType.withLabel(task.type!) == PendingTaskType.API_LOGOUT
+    }
+
+    override func deserialize(_ task: PendingTasks) -> String {
+        return ""
+    }
+}
+
+extension String: HandyJSON {
+}
